577865e754541c94a56fcd6a8a2772e5077960d8
GENIUS512 <Sulejmanozdoev220@Gmail.com>
2026-09-05 17:57:43 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,5 +3,11 @@
 ulimit -v 450000  # 450 МБ виртуальной памяти
 ulimit -t 600     # 10 минут CPU времени
 
-# Запускаем без ограничения конкурентности
+# Экспортируем переменные окружения
+export CUDA_VISIBLE_DEVICES=""
+export PYTORCH_ENABLE_MPS_FALLBACK="1"
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+
+# Запускаем
 exec uvicorn main:app --host 0.0.0.0 --port $PORT --workers 1
